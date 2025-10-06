46987c3ead441d0dd7fa20f73f5023f53ef8bce0
diegox0z <locobetax@gmail.com>
2025-10-06 10:32:59 +0200

    Quitando texto clicable

--- lib/screens/ejercicio1.dart
+++ lib/screens/ejercicio1.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:url_launcher/url_launcher.dart';  
 import 'package:relacion_ejercicios_flutter_diego_acosta/drawer/drawer_personalizado.dart';
 
 class Ejercicio1 extends StatelessWidget {
   const Ejercicio1({super.key});
-
-  // URL
-  static final Uri _url = Uri.parse('https://github.com/diegox0z/relacion_ejercicios_flutter_diego_acosta');
-
-  // Función para abrir la URL
-  Future<void> _launchUrl() async {
-    if (!await launchUrl(_url)) {
-      throw 'No se pudo abrir $_url';
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,9 +26,7 @@ class Ejercicio1 extends StatelessWidget {
             const SizedBox(height: 30),
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: GestureDetector(  // Hacemos el texto clickeable
-                onTap: _launchUrl,
-                child: Text(
+              child: Text(  
                   'https://github.com/diegox0z/relacion_ejercicios_flutter_diego_acosta',
                   style: GoogleFonts.robotoMono(
                     fontSize: 16,
@@ -48,7 +35,6 @@ class Ejercicio1 extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
               ),
-            ),
           ],
         ),
       ),
